010d617ca6d34770349db41571ab336812fcd322
7akub <jwolak@p100.io>
2023-05-12 15:25:14 +0200

    mlkit: fix conflicts with idenfy sdk

--- android/src/main/java/com/nateshmbhat/card_scanner/scanner_core/CardScanner.kt
+++ android/src/main/java/com/nateshmbhat/card_scanner/scanner_core/CardScanner.kt
@@ -6,6 +6,7 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.nateshmbhat.card_scanner.SingleFrameCardScanner
 import com.nateshmbhat.card_scanner.logger.debugLog
 import com.nateshmbhat.card_scanner.onCardScanFailed
@@ -50,7 +51,7 @@ class CardScanner(private val scannerOptions: CardScannerOptions, private val on
     if (mediaImage != null) {
       val image = InputImage.fromMediaImage(mediaImage, 90)
 
-      val recognizer = TextRecognition.getClient()
+      val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
       val result = recognizer.process(image)
               .addOnSuccessListener { visionText ->
